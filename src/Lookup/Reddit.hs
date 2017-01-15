b76518e3867e2ca4a2ec0b4b3b14db558370f70b
Jeffrey Dwyer <Jeffrey Dwyer>
2017-01-15 12:10:28 -0800

    Added utility functions for Reddit querying

--- src/Lookup/Reddit.hs
+++ src/Lookup/Reddit.hs
@@ -6,23 +6,37 @@ import Control.Monad
 import Control.Monad.IO.Class
 import Data.Monoid
 import Data.Default.Class
-import qualified Data.Text as Text
-import qualified Data.Text.IO as Text
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Data.Time.Clock
+import Data.Time.Calendar
 
 import Reddit
 import Reddit.Types.Post
 import Reddit.Types.Listing
 
-tshow :: Show a => a -> Text.Text
-tshow = Text.pack . show
+tshow :: Show a => a -> T.Text
+tshow = T.pack . show
 
-test :: MonadIO m => RedditT m ()
-test = do
-  Listing _ _ posts <- getPosts' def Top (Just . R $ "technology")
-  forM_ (take 10 $ posts) $ \post -> do
-    liftIO $ Text.putStrLn (title post)
+type RedditResult m a = m (Either (APIError RedditError) a)
 
-testr :: IO ()
-testr = do
-    r <- runRedditAnon test
-    print r
+fmap' f = fmap (fmap f)
+fmap'' f = fmap' (fmap f)
+
+pullPostsFrom :: MonadIO m => T.Text -> RedditResult m [Post]
+pullPostsFrom = runRedditAnon . fmap contents . getPosts' def Top . Just . R
+
+nPostsFrom :: MonadIO m => Int -> T.Text -> RedditResult m [Post]
+nPostsFrom n subName = fmap (take n <$>) (pullPostsFrom subName)
+
+postsSince :: UTCTime -> [Post] -> [Post]
+postsSince date = filter ((<) date . created)
+
+postsForThisWeek :: MonadIO m => T.Text -> RedditResult m [Post]
+postsForThisWeek subName = do
+    UTCTime d t <- liftIO getCurrentTime
+    posts <- pullPostsFrom subName
+    return $ postsSince (UTCTime (addDays (-7) d) t) <$> posts
+    
+titlesFromThisWeek :: MonadIO m => T.Text -> RedditResult m [T.Text]
+titlesFromThisWeek = fmap' (fmap title) . postsForThisWeek
